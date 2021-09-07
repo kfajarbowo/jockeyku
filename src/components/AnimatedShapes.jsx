5d0200128b164c85546d7bce905a033ba71254e3
kfajarbowo <kfajarbowo12@gmail.com>
2021-09-07 22:15:47 +0700

    Add : Add shape animation

---
+++ src/components/AnimatedShapes.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Square = styled.div`
+    width: 60px;
+    height: 60px;
+    background-color: #4e6bff;
+    opacity: 0.7;
+    position: absolute;
+    top: -60px;
+    left: -60px;
+    z-index: -1;
+    animation: square 10s linear alternate infinite;
+
+    @keyframes square{
+        to{
+            transform: translate(100vw, 100vh);
+        }
+    }
+`
+
+const Circle = styled.div`
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    background-color: #4eafff;
+    position: absolute;
+    top: 200px;
+    left: -100px;
+    z-index: -1;
+    animation: circle 10s linear alternate infinite;
+
+    @keyframes circle{
+        to{
+            transform: translate(100vw, -100vh);
+        }
+    }
+`
+
+const Rect = styled.div`
+    width: 50px;
+    height: 100px;
+    background-color: #5a4eff;
+    opacity: 0.5;
+    position: absolute;
+    top: 400px;
+    left: -50px;
+    z-index: -1;
+    animation: rect 15s linear alternate infinite;
+
+    @keyframes rect{
+        to{
+            transform: translate(100vw, -50vh);
+        }
+    }
+`
+
+const AnimatedShapes = () => {
+    return (
+        <>
+        <Square/>
+        <Circle/>
+        <Rect/>
+        </>
+    )
+}
+
+export default AnimatedShapes;
